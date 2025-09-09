da5254140f96632f54b65de3881a3b15e9482701
cerronefederico <cerronefederico.1@gmail.com>
2025-09-09 11:52:46 +0200

    b

--- dati.py
+++ dati.py
@@ -1,6 +1,8 @@
 from pymodbus.client import ModbusTcpClient
-client = ModbusTcpClient('192.168.1.100', port=502)
-if client.connect():
-    print("Connection established")
-else:
-    print("Connection failed")  
+client = ModbusTcpClient('192.168.0.1', port=102)
+
+try:
+    client.connect()
+    print("Connessione a PostgreSQL avvenuta con successo!")
+except Exception:
+    print("Connessione a PostgreSQL avvenuta con error!")
--- db.py
+++ db.py
@@ -2,9 +2,9 @@ import psycopg
 
 # Dati di connessione al database
 DB_HOST = "localhost"
-DB_NAME = "miodatabase"
-DB_USER = "mio_utente"
-DB_PASS = "mia_password"
+DB_NAME = "postgres"
+DB_USER = "user_admin"
+DB_PASS = "admin"
 
 try:
     # Connessione al database
@@ -14,14 +14,8 @@ try:
         # Creazione di un cursore
         with conn.cursor() as cur:
             # Esecuzione di una query
-            cur.execute("SELECT * FROM utenti;")
+            cur.execute("CREATE TABLE schema.a(num INTEGER PRIMARY KEY)")
 
-            # Recupero di tutti i risultati
-            righe = cur.fetchall()
-
-            # Stampa delle righe
-            for riga in righe:
-                print(riga)
 
 except psycopg.OperationalError as e:
     print(f"Errore di connessione al database: {e}")
